89e858bb05c27b59c72a302d0d291d4f310b607e
Carly H <carlyhublou@gmail.com>
2017-05-17 15:49:53 -0400

    Add files via upload

---
+++ Day08.java
@@ -0,0 +1,27 @@
+
+package day08;
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Map<String,Integer> friendPhone = new HashMap<String,Integer>();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            friendPhone.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(friendPhone.containsKey(s)){
+                System.out.println(s + "=" + friendPhone.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
