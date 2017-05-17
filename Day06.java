9693a53e79fd5a18e257cae29f7edb2af6f17184
Carly H <carlyhublou@gmail.com>
2017-05-17 15:46:26 -0400

    Add files via upload

---
+++ Day06.java
@@ -0,0 +1,31 @@
+package day06;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int numCases = scanner.nextInt();
+        int index = 0;
+                
+        while(index < numCases){
+            String even_odd = scanner.next();
+            char[] myCharArray = even_odd.toCharArray();
+            for(int j = 0; j < even_odd.length(); j=j+2){
+                System.out.print(myCharArray[j]);
+            }
+            System.out.print(" ");
+            for(int k = 1; k < even_odd.length(); k=k+2){
+                System.out.print(myCharArray[k]);
+            }
+            System.out.println();
+            index++;
+        }
+    }
+}
