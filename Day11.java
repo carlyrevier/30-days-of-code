c81e86bd66298801d81c4a23d7feaaac7a6feb3f
Carly H <carlyhublou@gmail.com>
2017-05-18 15:41:39 -0400

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,33 @@
+package day11;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int maxScore = -100000;
+                
+        for(int i=0; i<4; i++){
+            for (int j=0; j<4; j++){
+                int tempScore = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                if (tempScore > maxScore){
+                    maxScore = tempScore;
+                } else {
+                    tempScore = 0;
+                }
+            }
+        }
+        System.out.print(maxScore);
+    }
+}
